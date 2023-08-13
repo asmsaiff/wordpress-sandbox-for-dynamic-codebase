9fbe163e717da683874b2cdfc2ac4f6f9e7c4a67
asmsaiff <info@saifullah.co>
2023-08-13 18:31:35 +0600

    add tailwind element js and ajax code for submit image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
+; (function ($) {
+    jQuery(document).ready(function($) {
+        $('#file-upload-form').submit(function(e) {
+            e.preventDefault();
+
+            var formData = new FormData(this);
+
+            $.ajax({
+                url: aj.ajaxurl,
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    $('#upload-status').html(response);
+                }
+            });
+        });
+    });
+}(jQuery)); 
+
 initTE({ Collapse, Dropdown });
+initTE({ Tab });
